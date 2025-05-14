ea93721f9937b2e3645685b3181ed9c332d0718b
GregoryStam04 <gregory.stam04@gmail.com>
2025-05-14 04:14:42 +0300

    Traces for q4, almost final form

--- sql/queries/Q04.sql
+++ sql/queries/Q04.sql
@@ -1,5 +1,5 @@
 SET profiling = 1;
-
+SET join_cache_level = 2;
 -- Query 4: Average ratings for a specific artist
 EXPLAIN
 SELECT 
@@ -15,10 +15,8 @@ WHERE
     a.artist_id = 4  -- Replace with specific artist ID
 GROUP BY 
     a.artist_id, a.artist_name;
-SHOW PROFILES;
 
 -- Query 4 Alternative with force index:
-SET profiling = 1;
 EXPLAIN
 SELECT 
     a.artist_id,
@@ -33,10 +31,8 @@ WHERE
     a.artist_id = 4
 GROUP BY 
     a.artist_id, a.artist_name;
-SHOW PROFILES;
 
 -- Nested Loop Join
-SET profiling = 1;
 EXPLAIN
 SELECT /*+ JOIN_ORDER(a, p, r) JOIN_PREFIX(a, p USING(nested_loop)) */
     a.artist_id,
@@ -51,11 +47,28 @@ WHERE
     a.artist_id = 4
 GROUP BY 
     a.artist_id, a.artist_name;
-SHOW PROFILES;
 
+-- Hash Join BNLH
+SET join_cache_level = 4;
+SET SESSION optimizer_switch = 'join_cache_hashed=on,join_cache_bka=off';
+EXPLAIN /*+ HASH_JOIN(p) HASH_JOIN(r)*/
+SELECT 
+    a.artist_id,
+    a.artist_name,
+    AVG(r.artist_performance) AS avg_artist_performance,
+    AVG(r.overall_impression) AS avg_overall_impression
+FROM 
+    Artist a
+    INNER JOIN Performance p ON a.artist_id = p.artist_id
+    INNER JOIN Rating r ON p.performance_id = r.performance_id
+WHERE 
+    a.artist_id = 4
+GROUP BY 
+    a.artist_id, a.artist_name;
 
--- Hash Join
-SET profiling = 1;
+--Hash Join BKAH
+SET join_cache_level = 8;
+SET SESSION optimizer_switch = 'join_cache_hashed=on,join_cache_bka=off';
 EXPLAIN /*+ HASH_JOIN(p) HASH_JOIN(r)*/
 SELECT 
     a.artist_id,
